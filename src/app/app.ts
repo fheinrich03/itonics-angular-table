5414bdeea49930f891e27a9f9c8a315be0cbaf99
fheinrich <heinrichfelix03@gmail.com>
2026-01-29 19:31:30 +0100

    add ag grid basic config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Table } from './components/table/table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Table],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
